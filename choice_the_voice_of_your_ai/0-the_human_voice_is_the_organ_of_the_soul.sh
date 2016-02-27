e010c50ebdb14c1db7ee9d0fc76ac8cffbfde0bd
Daniel Alzugaray <dalzuga@gmail.com>
2016-02-27 11:15:46 -0800

    done

--- choice_the_voice_of_your_ai/0-the_human_voice_is_the_organ_of_the_soul.sh
+++ choice_the_voice_of_your_ai/0-the_human_voice_is_the_organ_of_the_soul.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-say $1
+
+FILENAME=$(echo $1 | awk '{print $1}')
+FILENAME+=.m4a
+
+case $2 in
+    m)
+	say -o $FILENAME -v Daniel $1;
+	;;
+    f)
+	say -o $FILENAME -v Agnes $1;
+	;;
+    x)
+	say -o $FILENAME -v Zarvox $1;
+	;;
+esac

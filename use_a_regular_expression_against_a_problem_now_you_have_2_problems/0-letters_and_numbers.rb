18bf69142e0a14ae36819f6060aafba81c899977
Daniel Alzugaray <dalzuga@gmail.com>
2016-03-18 20:28:35 -0700

    saving progress on task 1

--- use_a_regular_expression_against_a_problem_now_you_have_2_problems/0-letters_and_numbers.rb
+++ use_a_regular_expression_against_a_problem_now_you_have_2_problems/0-letters_and_numbers.rb
@@ -1,3 +1,2 @@
 #!/usr/bin/ruby
-#./0-letters_and_numbers.rb "ILoveSeaHorsesButNot*s"'!'"Tthe quick brown fox 1 2 3j' ; ;l l; ;2 1;l/..;..';.';.@#$%^&*()_hkjsdfh"
-r1 = ARGV[0].match(/[a-zA-Z\d]*/).to_s
+puts ARGV[0].scan(/[a-zA-Z\d]/).join
--- use_a_regular_expression_against_a_problem_now_you_have_2_problems/1-characters_space_characters.rb
+++ use_a_regular_expression_against_a_problem_now_you_have_2_problems/1-characters_space_characters.rb
@@ -1,1 +1,2 @@
 #!/usr/bin/ruby
+puts ARGV[0].match(/[a-zA-Z\d]* [a-zA-Z\d]*/)

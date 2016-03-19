be2aec3faa822a7779985ef1cc5c059cc722b525
Daniel Alzugaray <dalzuga@gmail.com>
2016-03-19 02:56:52 +0000

    starting task 0

---
+++ use_a_regular_expression_against_a_problem_now_you_have_2_problems/0-letters_and_numbers.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/ruby
+#./0-letters_and_numbers.rb "ILoveSeaHorsesButNot*s"'!'"Tthe quick brown fox 1 2 3j' ; ;l l; ;2 1;l/..;..';.';.@#$%^&*()_hkjsdfh"
+r1 = ARGV[0].match(/[a-zA-Z\d]*/).to_s

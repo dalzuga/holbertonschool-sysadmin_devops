69456e44eebd5672e69fedf6c8d4ec2033665251
Daniel Alzugaray <dalzuga@gmail.com>
2016-05-11 19:36:03 +0000

    task 2 & 3 done

---
+++ permissions/2-change_file_permissions_with_bits.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+touch /tmp/permissions
+chmod 504 /tmp/permissions
+ls -l /tmp/permissions
---
+++ permissions/3-change_file_permissions_with_letters.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+touch /tmp/permissions
+chmod ugo=rw /tmp/permissions
+ls -l /tmp/permissions
